ac31d888a8deeefc22b341f56847c0021640c747
dayoadediran <gistwithdayo@yahoo.com>
2021-04-16 08:57:45 -0400

    added redux to filter

--- src/App.js
+++ src/App.js
@@ -35,27 +35,27 @@ class App extends React.Component {
     this.setState({cartItems});
     localStorage.setItem("cartItems", JSON.stringify(cartItems));
   };
-  sortProducts=(e)=>{
-    const sort = e.target.value;
-    this.setState((state)=> ({
-      sort: sort,
-      products: this.state.products.slice().sort((a,b)=>(
-        sort === "lowest" ? ((a.price > b.price) ? 1:-1):
-        sort === "highest" ? ((a.price < b.price) ? 1:-1):
-        ((a._id < b._id) ? 1:-1)
-      ))
-    }));
-  }
-  filterProducts =(e)=> {
-    if(e.target.value===""){
-      this.setState({size:e.target.value, product: data.products})
-    }else{
-      this.setState({
-        size:e.target.value,
-        products: data.products.filter((product)=> product.availableSizes.indexOf(e.target.value) >=0)
-      })
-    }
-  }
+  // sortProducts=(e)=>{
+  //   const sort = e.target.value;
+  //   this.setState((state)=> ({
+  //     sort: sort,
+  //     products: this.state.products.slice().sort((a,b)=>(
+  //       sort === "lowest" ? ((a.price > b.price) ? 1:-1):
+  //       sort === "highest" ? ((a.price < b.price) ? 1:-1):
+  //       ((a._id < b._id) ? 1:-1)
+  //     ))
+  //   }));
+  // }
+  // filterProducts =(e)=> {
+  //   if(e.target.value===""){
+  //     this.setState({size:e.target.value, product: data.products})
+  //   }else{
+  //     this.setState({
+  //       size:e.target.value,
+  //       products: data.products.filter((product)=> product.availableSizes.indexOf(e.target.value) >=0)
+  //     })
+  //   }
+  // }
   removeFromCart=(product)=>{
     const cartItems = this.state.cartItems.slice();
     this.setState({
@@ -73,13 +73,16 @@ class App extends React.Component {
         <main>
           <div className="content">
             <div className="main">
-              <Filter count ={this.state.products.length}
-                      size = {this.state.size}
-                      sort = {this.state.sort}
-                      filterProducts = {this.filterProducts}
-                      sortProducts = {this.sortProducts}
+              <Filter 
+                      // count ={this.state.products.length}
+                      // size = {this.state.size}
+                      // sort = {this.state.sort}
+                      // filterProducts = {this.filterProducts}
+                      // sortProducts = {this.sortProducts}
               />
-              <Products products={this.state.products} addToCart={this.addToCart}/>
+              <Products 
+              // products={this.state.products} 
+              addToCart={this.addToCart}/>
             </div>
             <div className="sidebar">
               <Cart cartItems={this.state.cartItems} 
